2f0f1f3de9d0b2f9843d685f0d51ef6513867ed5
9cbell <31480938+9cbell@users.noreply.github.com>
2019-03-28 13:58:18 -0500

    Add files via upload

--- buy.php
+++ buy.php
@@ -5,11 +5,20 @@ $candycount = $_POST['candy'];
 $soda = $_POST['soda'];
 $sodacount = $_POST['soda'];
 
+//Login Information
+$usr = 'mrflemin_****';
+$pw = '***********';
+$db = 'mrflemin_****';
 
+// Create connection
+if (strlen($candy) > 0 or strlen($soda) > 0) {
+    $conn = new mysqli('localhost', $usr, $pw, $db);
+}
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
 
-
-
-
-
+$sql = "INSERT INTO tablename (candy, candycount, sodacount, soda) VALUES ('" . $candy . "', '" . $candycount . "', '" . $soda . "', '" . $sodacount . "')";
 
 ?>
