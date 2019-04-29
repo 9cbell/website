f41d3d2399198f7d08a22c81c0e0fef1d93c2148
9cbell <31480938+9cbell@users.noreply.github.com>
2019-04-29 13:57:49 -0500

    Add files via upload

--- buy.php
+++ buy.php
@@ -1,17 +1,20 @@
 <?php
-
-$candy = $_POST['candy'];
-$candycount = $_POST['candy'];
-$soda = $_POST['soda'];
-$sodacount = $_POST['soda'];
+//Grabs information from the previous html form
+//When you hit submit.
+$user = 1;
+$candy = (int)$_POST['candy'];
+$candycount = (int)$_POST['c_count'];
+$soda = (int)$_POST['soda'];
+$sodacount = (int)$_POST['s_count'];
 
 //Login Information
-$usr = 'mrflemin_****';
-$pw = '***********';
-$db = 'mrflemin_****';
+//DO NOT UPLOAD PASS TO GITHUB
+$usr = 'mrflemin_charlie';
+$pw = 'dF3,66%GgEcf';
+$db = 'mrflemin_charlieDB';
 
 // Create connection
-if (strlen($candy) > 0 or strlen($soda) > 0) {
+if ($candycount > 0 or $sodacount > 0) {
     $conn = new mysqli('localhost', $usr, $pw, $db);
 }
 // Check connection
@@ -19,9 +22,18 @@ if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
 
-$sql = "INSERT INTO tablename (candy, candycount, sodacount, soda) VALUES ('" . $candy . "', '" . $candycount . "', '" . $soda . "', '" . $sodacount . "')";
+$sql = "INSERT INTO history (date, user_id, item_id, quantity ) VALUES  (CURDATE(), {$user}, {$soda}, {$sodacount})";
 
 if ($conn->query($sql) === true) {
+    echo "Successful Order!";
+    echo "<a href=\"MainPage.html\" onclick=\"location.reload()\">Go back to home page</a>";
+} else {
+    echo 'Error: ' . $sql . '<br>' . $conn->error;
+}
+
+$sql2 = "INSERT INTO history (date, user_id, item_id, quantity ) VALUES (CURDATE(), {$user}, {$candy}, {$candycount})";
+
+if ($conn->query($sql2) === true) {
     echo "Successful Order!";
     echo "<a href=\"MainPage.html\" onclick=\"location.reload()\">Go back to home page</a>";
 } else {
@@ -30,3 +42,5 @@ if ($conn->query($sql) === true) {
 $conn->close();
 
 ?>
+
+
